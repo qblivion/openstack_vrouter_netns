b04f235831b91e2c87522fa2c3dc515732588e85
qblivion <rsadykov944@gmail.com>
2024-04-18 09:27:40 +0300

    Add files via upload

---
+++ opencontrail-vrouter-netns/opencontrail_vrouter_netns/cert_mgr/cert_manager.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+from builtins import object
+import logging
+
+#check python version
+if six.PY2:
+    import exceptions
+else:
+    import builtins as exceptions
+
+try:
+    from .barbican_cert_manager import BarbicanCertManager
+except ImportError:
+    pass
+
+try:
+    from .kubernetes_cert import KubernetesCert
+except ImportError:
+    pass
+
+try:
+    from .cloudstack_cert_manager import CloudstackCertManager
+except ImportError:
+    pass
+
+class CertManager(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def update_ssl_config(orchestrator, provider, \
+        haproxy_config, auth_conf, dest_dir):
+        if orchestrator == 'openstack':
+            if provider == 'barbican':
+                updated_config = (BarbicanCertManager(auth_conf)). \
+                    update_ssl_config(haproxy_config, dest_dir)
+        elif orchestrator == 'cloudstack':
+            updated_config = (CloudstackCertManager(auth_conf)). \
+                update_ssl_config(haproxy_config, dest_dir)
+        elif orchestrator == 'kubernetes':
+            updated_config = (KubernetesCert(auth_conf)). \
+                update_ssl_config(haproxy_config, dest_dir)
+        return updated_config
